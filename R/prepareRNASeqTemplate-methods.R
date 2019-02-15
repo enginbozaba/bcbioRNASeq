4d57b930484f72943e9180b53b9888e0a351a588
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-15 11:16:39 -0500

    Update `prepareRNASeqTemplate()` to match new basejump convention

--- R/prepareRNASeqTemplate-methods.R
+++ R/prepareRNASeqTemplate-methods.R
@@ -1,34 +1,12 @@
-#' Prepare RNA-Seq R Markdown Template
+#' Prepare RNA-seq R Markdown Template
 #'
 #' @name prepareRNASeqTemplate
-#' @family R Markdown Functions
 #' @author Michael Steinbaugh
-#'
 #' @inherit basejump::prepareTemplate
-#'
 #' @export
 #'
 #' @examples
-#' x <- prepareRNASeqTemplate()
-#' x
-#'
-#' # Clean up
-#' unlink(c(
-#'     "_footer.Rmd",
-#'     "_header.Rmd",
-#'     "_output.yaml",
-#'     "_setup.R",
-#'     "bibliography.bib"
-#' ))
+#' ## x <- prepareRNASeqTemplate()
 prepareRNASeqTemplate <- function(overwrite = FALSE) {
-    package <- "bcbioRNASeq"
-    prepareTemplate(
-        package = package,
-        sourceDir = system.file(
-            "rmarkdown/shared",
-            package = package,
-            mustWork = TRUE
-        ),
-        overwrite = overwrite
-    )
+    prepareTemplate(package = "bcbioRNASeq", overwrite = overwrite)
 }
